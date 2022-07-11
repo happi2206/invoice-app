ec8d25ebe761811d89423851f9f755cf69cb0425
happi2206 <happinessadeboye@gmail.com>
2022-07-11 22:12:38 +0100

    feat: :art:

---
+++ my-invoice/src/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="w-16 h-screen">
+    <div class="flex z-50 mainbg rounded-tr-md rounded-br-md">
+      <h2 class="text-center text-xs text-white p-2">Create Invoice</h2>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ my-invoice/src/views/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container px-5">
+    <div class="flex">
+      <div class="flex flex-col">
+        <h1>Invoices</h1>
+        <span>There are 4 total invoices</span>
+      </div>
+      <div class="flex">
+        <div @click="toggleFilter" class="flex" ref="filter">
+          <span>Filter by status</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
